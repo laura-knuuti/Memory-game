dd3be467b38b16c1f60d09cac9f838cbba5a8d1a
laura-knuuti <laura.knuuti@gmail.com>
2024-12-01 20:00:29 +0200

    Add files via upload

---
+++ Muistipeli.js
@@ -0,0 +1,58 @@
+/*number 1 */
+
+/*const card = document.getElementById('.card')
+
+card.addEventListener("click", flipCard);
+
+let front = document.getElementsByClassName('front');
+
+front.addEventListener("click", flipCard);
+
+function flipCard() {
+
+    card.classList.toggle('flipCard');
+
+}*/
+
+/*number 2*/
+
+let card = document.getElementById('p1')
+
+card.addEventListener("click", flipCard);
+
+function flipCard() {
+
+    card.classList.toggle('flipCard');
+
+}*/
+
+/*number 3*/
+
+/*let card = document.getElementsByClassName('back')
+
+card.addEventListener("click", flipCard);
+
+let front = document.getElementsByClassName('front');
+
+front.addEventListener("click", flipCard);
+
+function flipCard() {
+
+    card.classList.toggle('flipCard'); }*/
+
+
+/*number 4*/
+
+/*    const card = document.getElementById('p1')
+
+card().addEventListener("click", flipCard);
+
+let front = document.getElementsByClassName('front');
+
+front().addEventListener("click", flipCard);
+
+function flipCard() {
+
+    card().classList.toggle('flipCard');*/
+
+}
